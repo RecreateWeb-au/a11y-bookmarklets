2a8d08ed35b71941c2dd71088e32c08f2c558df6
Adem Cifcioglu <ademciftcioglu@gmail.com>
2014-06-25 19:50:13 +1000

    Updated page styles

---
+++ javascripts/common.js
@@ -0,0 +1,6 @@
+document.createElement("header");
+document.createElement("footer");
+document.createElement("nav");
+document.createElement("section");
+document.createElement("article");
+document.createElement("aside");
